b22f267834772eeeb5de93d41764a5f8f0e1e38e
luxeivan <luxeivan@gmail.com>
2022-06-26 19:07:57 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const { WebSocketServer } = require('ws');
+let openUsers = [];
+
+const wss = new WebSocketServer({ port: 8081 });
+
+
+wss.on('connection', (ws) => {
+  ws.username = '';
+  ws.on('message', data => {
+    const obj = JSON.parse(data);
+    console.log('received: %s', data);
+    if (obj.register) {
+      if (!openUsers.includes(obj.register, 0)) {
+        openUsers.push(obj.register);
+        ws.username = obj.register;
+        console.log(ws.username);
+        ws.send(JSON.stringify({ openUsers, currentUser: obj.register }));
+        Array.from(wss.clients).filter(o => o.readyState === ws.OPEN).forEach(o => o.send(JSON.stringify({ openUsers })));
+      } else {
+        console.log(wss.clients);
+        ws.send(JSON.stringify({ userIncorrect: true }));
+      }
+    } else {
+      Array.from(wss.clients).filter(o => o.readyState === ws.OPEN).forEach(o => o.send(JSON.stringify(obj)));
+    }
+
+  });
+
+
+  //ws.send(JSON.stringify({ user: 'Dima', message: 'GQwerty' }));
+});
+setInterval(() => {
+  const newListUser = [];
+  Array.from(wss.clients).filter(o => o.readyState === 1).forEach(o => {
+    newListUser.push(o.username);
+  });
+  openUsers.forEach((item,index)=>{
+    if(!newListUser.includes(item,0)){
+      openUsers.splice(index,1)
+      Array.from(wss.clients).filter(o => o.readyState === 1).forEach(o => o.send(JSON.stringify({ openUsers })));
+    }
+  });
+}, 1000);
+console.log('start on 8081');
---
+++ frontend/src/js/app.js
@@ -0,0 +1,6 @@
+import Chat from "./Chat";
+import Ws from "./Ws";
+
+const wsConnect = new Ws('ws://192.168.1.112:8081');
+const chat = new Chat('chat-area', wsConnect);
+wsConnect.addChat(chat);

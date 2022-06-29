7c2561c5692f65bf7c286eb493e6667bdb114b7a
luxeivan <luxeivan@gmail.com>
2022-06-29 11:55:19 +0300

    123

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -2,6 +2,6 @@ import Chat from "./Chat";
 import Ws from "./Ws";
 
 //const wsConnect = new Ws('ws://192.168.1.112:8081');
-const wsConnect = new Ws('wss://ahj-chat-back.vercel.app/8081');
+const wsConnect = new Ws('wss://chatback1.herokuapp.com:8081');
 const chat = new Chat('chat-area', wsConnect);
 wsConnect.addChat(chat);
